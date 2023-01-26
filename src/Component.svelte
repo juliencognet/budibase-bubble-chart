7457ddd6b8c32b2811421f772ca6d32c49fa8321
COGNET, JULIEN <julien.cognet@cgi.com>
2023-01-26 23:59:14 +0100

    Fixed errors, added configuration check

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,20 +1,19 @@
 <script>
-  import { getContext, setContext } from "svelte"
+  import { getContext } from "svelte"
   import { chart } from "svelte-apexcharts";
   import { merge } from 'lodash';
-  const loading = getContext("loading")
-
-  export let dataProvider
+  
+  export let dataProvider //
   export let serieField //
   export let xAxisField // 
   export let yAxisField //
   export let bubbleSizeField // 
   export let xAxisType //
   export let chartHeight //
+  export let chartWidth //
   export let chartTitle //
   export let minXAxis //
   export let maxXAxis //
-  export let xAxisTitle //
   export let xAxisTickAmount //
   export let minYAxis //
   export let maxYAxis //
@@ -25,70 +24,149 @@
   export let colorPalette //
   export let advancedChartOptions //
 
+  const loading = getContext("loading")
+  let canBeDisplayed = true;
+  let errorMessages = [];
+
+  // ----------------------------------------------------------------
+  // Check configuration of chart
+  if (dataProvider==null){
+    errorMessages.push("Data provider must be defined.");
+  }
+  const xAxisAuthorizedFieldTypesAndMapping = [
+    {"fieldType":"datetime","mappedChartType":"datetime"}
+   ,{"fieldType":"number","mappedChartType":"numeric"}
+   ,{"fieldType":"string","mappedChartType":"category"}
+   ,{"fieldType":"barcodeqr","mappedChartType":"category"}
+   ,{"fieldType":"options","mappedChartType":"category"}
+   ]
+  const errorIfNotInAuthorizedTypes = "must be either a date, a number, a string, options or a barcode"
+  if (serieField!=null){
+    if (xAxisAuthorizedFieldTypesAndMapping.find(elt=>elt.fieldType==dataProvider.schema[serieField].type)===undefined){
+      errorMessages.push("Serie field "+errorIfNotInAuthorizedTypes);
+    }
+  }
+  if (xAxisField==null) {
+    errorMessages.push("xAxis Field must be defined");
+  } else if (xAxisAuthorizedFieldTypesAndMapping.find(elt=>elt.fieldType==dataProvider.schema[xAxisField].type)===undefined){
+    errorMessages.push("xAxis Field "+errorIfNotInAuthorizedTypes);
+  } else {
+    xAxisType = xAxisAuthorizedFieldTypesAndMapping.find(elt=>elt.fieldType==dataProvider.schema[xAxisField].type).mappedChartType;
+  }
+  if (yAxisField==null) {
+    errorMessages.push("yAxis Field must be defined");
+  } else if (dataProvider.schema[yAxisField].type != 'number') {
+    errorMessages.push("yAxis Field must be a number field")
+  } 
+  if (bubbleSizeField==null){
+    errorMessages.push("Bubble size field must be defined");
+  } else if (dataProvider.schema[bubbleSizeField].type != 'number') {
+    errorMessages.push("Bubble size field must be a number field");
+  }
+  if (errorMessages.length>0){
+    canBeDisplayed=false;
+  }
+
+  // ----------------------------------------------------------------
   // Manage all options of the chart
   var options = {};
   options.chart = { height: chartHeight, type: 'bubble', toolbar: {show: false}}
-  if (chartTitle){
+  if (chartWidth!=null){
+    options.chart.width = chartWidth;
+  }
+  if (chartTitle!=null){
     options.title = { text: chartTitle }
   }
-  options.xaxis = { type: xAxisType };
-  if (xAxisTitle){
-    options.xaxis.title = { text: xAxisTitle, offsetY: +80 };
-  }
-  if (minXAxis){
+  options.xaxis = { type: xAxisType }
+  if (minXAxis!=null){
     options.xaxis.min = minXAxis;
   }
-  if (maxXAxis){
+  if (maxXAxis!=null){
     options.xaxis.max = maxXAxis;
   }
-  if (xAxisTickAmount){
+  if (xAxisTickAmount!=null){
     options.xaxis.tickAmount = xAxisTickAmount;
   }
   options.yaxis = {};
-  if (yAxisTitle){
+  if (yAxisTitle!=null){
     options.yaxis.title = { text: yAxisTitle };
   }
-  if (minYAxis){
+  if (minYAxis!=null){
     options.yaxis.min = minYAxis;
   }
-  if (maxYAxis){
+  if (maxYAxis!=null){
     options.yaxis.max = maxYAxis;
   }
-  if (yAxisTickAmount){
+  if (yAxisTickAmount!=null){
     options.yaxis.tickAmount = yAxisTickAmount;
   }
   options.dataLabels = { enabled: displayValues };
   options.fill = { opacity : bubbleOpacity/100 };
   options.theme = {palette : colorPalette };
-  // Merge options
+
+  // Merge advanced chart options with options computed just before
   if (advancedChartOptions!=null){
     options = merge(options, advancedChartOptions)
   }
+
+  // ----------------------------------------------------------------
   // Manage data of the chart
   options.series = [];
   if (!serieField){
     options.series.push({"name":"DEFAULT",data: []});
   }
-  $: { // asynchronous portion of code which can be executed several times
-       // until data are loaded
-    let rows = $loading
-    ? new Array(dataProvider?.limit > 20 ? 20 : dataProvider?.limit).fill({})
-    : dataProvider?.rows
+
+  // Handle data when rows retrieved
+  function handleData(rows){
     let index=0;
-    for (const element of rows){
-        if (serieField == null){
-          index=0;
-        } else {
-          let categoryName = element[serieField];
-          index = options.series.findIndex( el => el.name == categoryName);
-          if (index==-1){
-            index = options.series.push({"name": categoryName, data: []}) -1 ;
+    if (rows){
+      for (const element of rows){
+          // Manage serie if defined (else will be DEFAULT value)
+          if (serieField == null){
+            index=0;
+          } else {
+            let categoryName = element[serieField];
+            index = options.series.findIndex( el => el.name == categoryName);
+            if (index==-1){
+              index = options.series.push({"name": categoryName, data: []}) -1 ;
+            }
           }
-        }
-        options.series[index].data.push({x:element[xAxisField],y:element[yAxisField],z:element[bubbleSizeField]});
-    };
-    console.log(options);
+          // If no value defined for x axis, but that axis if category type, then we can do something, else value will be ignored
+          if (element[xAxisField]!==undefined || options.xaxis.type=="category") {
+            let xValue = (element[xAxisField]===undefined) ? "<undefined>" : element[xAxisField]
+            options.series[index].data.push(
+              {x: xValue
+              ,y:element[yAxisField]
+              ,z:element[bubbleSizeField]});
+          }
+      };
+    }
   }
+  $: rows = $loading
+    ? new Array(dataProvider?.limit > 20 ? 20 : dataProvider?.limit).fill({})
+    : dataProvider?.rows;
+  $: if (dataProvider?.rows && dataProvider?.rows.length > 0){
+      handleData(dataProvider?.rows);
+  } 
 </script>
 
-<div use:chart={options} />
+{#if canBeDisplayed}
+  <div use:chart={options} />
+{:else}
+<!-- Display error message of bad configuration -->
+<div class="spectrum-InLineAlert spectrum-InLineAlert--notice">
+  <div class="spectrum-InLineAlert-header">
+    Bubble chart configuration error
+    <svg class="spectrum-Icon spectrum-Icon--sizeM spectrum-InLineAlert-icon" focusable="false" aria-hidden="true">
+      <use xlink:href="#spectrum-icon-18-Alert" />
+    </svg>
+  </div>
+  <div class="spectrum-InLineAlert-content">
+    <ul>
+      {#each errorMessages as message}
+        <li>{message}</li>
+      {/each}
+    </ul>
+  </div>
+</div>
+{/if}
